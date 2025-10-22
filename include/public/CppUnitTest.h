59824739d59ffcb027b336e41b9d16422e82fd16
ダニ <58553089+HFDan@users.noreply.github.com>
2025-10-22 21:10:36 +0300

    Fixed CMakeLists, added symbol demangling

--- include/public/CppUnitTest.h
+++ include/public/CppUnitTest.h
@@ -1,6 +1,22 @@
 #pragma once
+#include <memory>
+
 #include <TestRegistry.h>
 #include <CppUnitTestAssert.h>
+#if !__has_include("cxxabi.h")
+#error "Library cxxabi.h must be available"
+#endif
+#include <cxxabi.h>
+
+static inline std::string demangle(const char* name) {
+    int status = -1;
+    const std::unique_ptr<char, void(*)(void*)> result {
+        abi::__cxa_demangle(name, NULL, NULL, &status),
+        std::free
+    };
+
+    return (status == 0) ? std::string(result.get()) : name;
+}
 
 #define TEST_CLASS(CLASS_NAME)\
 class CLASS_NAME##_impl;\
@@ -13,12 +29,10 @@ class CLASS_NAME##_impl: public CLASS_NAME
 #define TEST_METHOD(METHOD_NAME)\
 struct AutoRegister_##METHOD_NAME {\
     AutoRegister_##METHOD_NAME() {\
-        std::string fullName = typeid(real_name).name();\
-        fullName.erase(fullName.end()-5, fullName.end());\
-        while (fullName[0] >= '0' && fullName[0] <= '9') {\
-            fullName.erase(fullName.begin());\
-        }\
-        fullName += ": " #METHOD_NAME;\
+        std::string fullName = demangle(typeid(real_name).name());\
+        fullName.erase(fullName.end() - 5, fullName.end());\
+        fullName += ": ";\
+        fullName += #METHOD_NAME;\
         auto& inst = Microsoft::VisualStudio::CppUnitTestFramework::TestRegistry::instance();\
         inst.Register(fullName,[]() -> void {\
             real_name instance;\
